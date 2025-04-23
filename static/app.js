5c0e5ab5b3eaa14d3038a6af94544e81f8fbcade
Atip Chananantachot <achananantachot@Atips-MacBook-Pro.local>
2025-04-23 11:57:34 +0700

    added support menu bar and footer

--- static/app.js
+++ static/app.js
@@ -45,3 +45,5 @@ function updateChart() {
 }
 updateChart()
 setInterval(updateChart, 5000);
+
+document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'));
