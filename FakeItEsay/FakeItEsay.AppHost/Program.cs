46d6d548f4f838394378f78d3a4cf03a2f25a3b4
Atip Chananantachot <47235392+Chananantachot@users.noreply.github.com>
2025-04-30 17:26:48 +0700

    Update Program.cs relate to new folder

--- FakeItEsay/FakeItEsay.AppHost/Program.cs
+++ FakeItEsay/FakeItEsay.AppHost/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 #pragma warning disable ASPIREHOSTINGPYTHON001
-var fakeItEasy = builder.AddPythonApp("FakeITEasy", "../../FakeITEasy", "app.py")
+var fakeItEasy = builder.AddPythonApp("FakeITEasy", "../../src", "app.py")
     .WithHttpEndpoint(env: "PORT", port: 5001)
     .WithHttpsEndpoint(env: "HTTPS_PORT", port: 5002);
    // .WithExternalHttpEndpoints()
