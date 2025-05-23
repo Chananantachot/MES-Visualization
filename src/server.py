c420757bad30cf0ba21c5c853405751b42da0b01
Atip Chananantachot <achananantachot@Atips-MacBook-Pro.local>
2025-05-23 12:15:01 +0700

    code clean-up

--- src/server.py
+++ src/server.py
@@ -1,63 +1,52 @@
 import time
-from opcua import Server
-import numpy as np
-import random
-import json
 import os
+import json
+from opcua import Server
+from mes import mes
 
+# Initialize MES and OPC UA Server
+mes_instance = mes()
 server = Server()
 server.set_endpoint("opc.tcp://0.0.0.0:4840/server/")
 server.set_server_name("MES OPC UA Server")
 server.start()
 
-# get Objects node, this is where we should put our nodes
+# Get Objects node and register namespace
 objects = server.get_objects_node()
 uri = "http://examples.freeopcua.github.io"
 idx = server.register_namespace(uri)
 
-# Create Products folder
+# Create folders
 products_folder = objects.add_folder(f"ns={idx};s=Products", "Products")
-# Create Senser folder
-sensers_folder = objects.add_folder(f"ns={idx};s=Sensors", "Sensors")
-# Create Motor folder
-motor_folder = objects.add_folder(f"ns={idx};s=Motors", "Motors")
+sensors_folder = objects.add_folder(f"ns={idx};s=Sensors", "Sensors")
+motors_folder = objects.add_folder(f"ns={idx};s=Motors", "Motors")
 
-# Craate Motor object
-mortor = motor_folder.add_object(f"ns={idx};s=Motor", "Motor")
-temperature = np.linspace(20, 100, 50)
-temp = 5000 - (temperature * 30)
-
-motor_temperature = mortor.add_variable(idx, "temperatures", temperature.tolist())
+# Create Motor object and variables
+motor = motors_folder.add_object(f"ns={idx};s=Motor", "Motor")
+motor_temperature = motor.add_variable(idx, "Temperatures", mes_instance.temperature.tolist())
 motor_temperature.set_writable()
-
-motor_speed = mortor.add_variable(idx, "speeds", temp.tolist() + np.random.normal(0, 100, size=50).tolist())
+motor_speed = motor.add_variable(idx, "Speeds", mes_instance.motor_speed.tolist())
 motor_speed.set_writable()
 
-# Craate Senser object
+# Create Sensor objects and variables
 for i in range(1, 4):
-    sensor = sensers_folder.add_object(f"ns={idx};s=Sensor{i}", f"Sensor{i}")
-    sensor_temperature = sensor.add_variable(idx, "Temperatures", [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    sensor = sensors_folder.add_object(f"ns={idx};s=Sensor{i}", f"Sensor{i}")
+    sensor_temperature = sensor.add_variable(idx, "Temperatures", mes_instance.sensor_temperatures)
     sensor_temperature.set_writable()
-    if i == 1:
-        sensor_signal = sensor.add_variable(idx, "Signal",  np.append(np.random.normal(600,  random.uniform(20, 50), 980), [200, 300]).tolist())
-    elif i == 2:
-        sensor_signal = sensor.add_variable(idx, "Signal",  np.append(np.random.normal(400, random.uniform(5, 50), 980), [100, 600]).tolist())
-    else:
-        sensor_signal = sensor.add_variable(idx, "Signal",  np.append(np.random.normal(200, random.uniform(10, 50), 980), [200, 800]).tolist())        
+    sensor_signal = sensor.add_variable(idx, "Signal", mes_instance.generate_signal_data())
     sensor_signal.set_writable()
-    
-# Craate Production Rates object
-path = os.path.join("static", "data", "MOCK_DATA.json")
-with open(path, "r") as f:
+
+# Create Product objects and variables
+data_path = os.path.join("static", "data", "MOCK_DATA.json")
+with open(data_path, "r") as f:
     data = json.load(f)
-    
-    unique_entries = {}
+    unique_products = {}
     for item in data:
-        if item['product_name'] not in unique_entries:
-            unique_entries[item['product_name']] = item
-    
-    for item in unique_entries.values():
-        product = products_folder.add_object(f"ns={idx};s={item['product_name']}", item['product_name'])
+        pname = item['product_name']
+        if pname not in unique_products:
+            unique_products[pname] = item
+    for item in unique_products.values():
+        product = products_folder.add_object(idx, item['product_name'])
         product_rate = product.add_variable(idx, "ProductRate", item['productRate'])
         product_rate.set_writable()
 
